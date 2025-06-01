f179b14578dfef355258c7309255e7186f67b0b2
Aaron Diehl <aaron.e.diehl@gmail.com>
2025-05-31 22:03:25 -0400

    Init

--- src/TT.Metalium/obj/Debug/net9.0/TT.Metalium.AssemblyInfo.cs
+++ src/TT.Metalium/obj/Debug/net9.0/TT.Metalium.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TT.Metalium")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d68e200017db493fe52e04f42d524cca09b304")]
 [assembly: System.Reflection.AssemblyProductAttribute("TT.Metalium")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TT.Metalium")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
